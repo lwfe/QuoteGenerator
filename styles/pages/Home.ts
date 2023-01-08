f15f41a24bbf64f78edcf6f810fe8a6dc7d5801b
lwfe <leonardolwfe@gmail.com>
2023-01-08 15:29:49 -0300

    finished quote generator

---
+++ styles/pages/Home.ts
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import { devices } from "../devices";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h2 {
+    color: ${(props) => props.theme.colors.primary};
+    font-size: 32px;
+    text-align: center;
+    margin-bottom: 40px;
+  }
+
+  button {
+    width: 200px;
+    margin-top: 30px;
+    outline: none;
+    background: ${(props) => props.theme.colors.primary};
+    padding: 12px;
+    border: none;
+    border-radius: 2px;
+    color: ${(props) => props.theme.colors.background};
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  button:hover {
+    cursor: pointer;
+    background: ${(props) => props.theme.colors.secondary};
+  }
+`;
+
+export const QuoteCard = styled.div`
+  @media ${devices.mobileS} {
+    width: 100%;
+    padding: 2rem;
+    background: ${(props) => props.theme.colors.backgroundSecondary};
+
+    h1 {
+      color: ${(props) => props.theme.colors.primary};
+      font-size: 25px;
+      text-align: right;
+    }
+
+    p {
+      margin-bottom: 12px;
+      font-size: 24px;
+      line-height: 32px;
+      text-align: justify;
+    }
+  }
+
+  @media ${devices.tablet} {
+    width: 80%;
+    border-radius: 8px;
+  }
+
+  @media ${devices.laptop} {
+    width: 50%;
+    border-radius: 8px;
+  }
+
+  @media ${devices.laptopL} {
+    width: 40%;
+    border-radius: 8px;
+  }
+`;
---
+++ styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    background: "#2d3436",
+    backgroundSecondary: "#636e72",
+    primary: "#fdcb6e",
+    secondary: "#ffeaa7",
+    text: "#dfe6e9",
+  },
+};
+
+export default theme;
